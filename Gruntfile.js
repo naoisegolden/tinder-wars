85729e1def6c03836da47de9ff51abaad0743878
Naoise Golden <naoise.golden@xing.com>
2015-02-12 21:24:15 +0100

    Styles for swipe animation and mobile view

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         files: {
-         'build/main.css':'sass/main.scss'
+         'build/main.css': 'sass/main.scss'
         }
       }
     },
